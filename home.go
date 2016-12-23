fcdec4c9cfa373830035bfa47454b0d5642a0bae
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-12-24 00:28:03 +0100

    +

--- home.go
+++ home.go
@@ -39,7 +39,6 @@ func Usage(e int, msg ...interface{}) {
 
 func main() {
     remain := getOpts()
-    dir.Ignore = ignoreDirs // []string{}
     // if debug {
     //     fmt.Printf("%s %s: %s -> %s\n", verbose, act, src, dst)
     // }
@@ -96,8 +95,8 @@ func getOpts() []string {
             case "t":
                 dst = opt.String()
 
-            // case "I", "R":
-            //     act = opt.String()
+            case "I", "R":
+                act = opt.String()
         }
     }
 
@@ -114,7 +113,84 @@ func getOpts() []string {
     return optarg.Remainder
 }
 
+func check(path string, info os.FileInfo, err error) error {
+    // fmt.Printf("%s?\n", path)
+    if err != nil {
+        return err
+    }
+    if !info.IsDir() {
+        // if verbose > 0 { fmt.Printf("Not a directory: %s\n", path) }
+        return dir.Skip
+    }
+    name := info.Name()
+    for _, i := range ignoreDirs {
+        // if regexp.MustCompile(r).MatchString(p.Name())
+        if name == i {
+            // fmt.Println(name, "ignored")
+            return dir.Skip
+        }
+    }
+    if strings.HasPrefix(name, "os_") {
+        if name == "os_" + OS {
+            // fmt.Println("filepath.Walk", path, checkDir)
+            err := walk(path)
+            if err != nil {
+                return err
+            }
+        }
+        return dir.Skip
+    }
+    return nil
+}
 
+func visit(path string, info os.FileInfo, e error) error {
+    if e != nil {
+        return e
+    }
+    // if exists(join(path, ".pkg")) {
+    //     fmt.Println(path, "READ PKG")
+    // }
+    // d := join(path, info.Name())
+    d, err := dir.Read(path)
+    if err != nil {
+        return err
+    }
+    if verbose > 0  {
+        fmt.Printf("DIR %s\n", join(path))
+    }
+    FILES:
+    for _, fi := range d {
+        switch filepath.Ext(fi.Name()) {
+            case ".tpl", ".pkg":
+                continue FILES
+        }
+        // for _, i := range ignore { }
+        s := join(path, fi.Name())
+        t := join(dst, fi.Name())
+        if verbose > 0  {
+            // fmt.Printf("%s <- %s\n", t, fi.Name())
+            fmt.Printf("ln -s %s %s\n", s, t)
+        }
+        // err := os.Symlink(s, t)
+        // if err != nil {
+        //     return err
+        // }
+        // realpath, err := filepath.EvalSymlinks(t)
+    }
+    return nil
+}
+
+func walk(path string) error {
+    return dir.Walk(path, check, visit)
+    // if err != nil {
+    //     return err
+    // }
+    // return nil
+}
+
+func join(paths ...string) string {
+    return filepath.Join(paths...)
+}
 
 func exists(path string) bool {
     if _, err := os.Stat(path); err != nil {
@@ -125,8 +201,4 @@ func exists(path string) bool {
         return false
     }
     return true
-}
-
-func join(paths ...string) string {
-    return filepath.Join(paths...)
 }
